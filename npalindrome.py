861d9f6f5377c3fd0b4f9fc25660eb5464690171
JetVolcano <184006209+JetVolcano@users.noreply.github.com>
2025-05-04 10:48:07 -0700

    Refactor numberpalindromes function to handle edge cases and improve type hinting

--- npalindrome.py
+++ npalindrome.py
@@ -1,12 +1,17 @@
 # This is a problem my math teacher gave me
 
-from math import floor, inf
+from math import floor, isinf
 
 
-def numberpalindromes(digits: int) -> float | str:
-  if float(f"9e+{floor((digits+1)/2)-1}") != inf:
+def numberpalindromes(digits: int) -> float | str | None:
+  if digits == 0:
+    return None
+  if digits < 0:
+    digits = abs(digits)
+  result: float = float(f"9e+{floor((digits+1)/2)-1}") 
+  if isinf(result) == False:
     return float(f"9e+{floor((digits+1)/2)-1}")
-  if float(f"9e+{floor((digits+1)/2)-1}") == inf:
+  if isinf(result):
     return f"9e+{floor((digits+1)/2)-1}"
-    
-print(numberpalindromes(9101065418293857667843617946356856574656746456785835767582976387597678754765878678595878947678977893767897667689765789087654323456789087654323456786543567654323456786543234567654324567876543456786543567865434567543456543456433453456434))
+
+print(numberpalindromes(0))
