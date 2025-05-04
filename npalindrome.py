3b551788c3f34a8de71a0c020f3a3d54953451b8
JetVolcano <184006209+JetVolcano@users.noreply.github.com>
2025-05-04 13:30:48 -0700

    Refactor NP class to improve method organization and enhance readability

--- npalindrome.py
+++ npalindrome.py
@@ -4,6 +4,7 @@ from math import isinf
 
 class NP():
   def __init__(self, digits: int, print_at_start: bool = False) -> None:
+    self.digits: int = digits
     self.result: float | str
     result: str = f"9e{(digits+1)//2-1}"
     if digits == 0:
@@ -16,6 +17,19 @@ class NP():
       self.result = float(result)
     if print_at_start == True:
       print(result)
+  def __eq__(self, value) -> bool:
+    return self.result == value
+  def __or__(self, value) -> bool:
+    return self.result or value
+  def __str__(self) -> str:
+    return str(self.digits)
+  def __len__(self) -> int:
+    return len(self.result)
+  def __repr__(self) -> repr:
+    return repr({
+      "digits": self.digits,
+      "result": self.result
+    })
     
     
 NP(9076545676543456523456786545676532456786543567543567546754675435675467546234567898765456786567655543456543213454567887654567, True)
